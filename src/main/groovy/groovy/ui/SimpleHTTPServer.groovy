0e1151f4e0b809a8ac011e51ce2768e7a452bfb2
Daniel Sun <sunlan@apache.org>
2016-12-13 20:51:07 +0800

    Fix minor issues

--- src/main/groovy/groovy/ui/SimpleHTTPServer.groovy
+++ src/main/groovy/groovy/ui/SimpleHTTPServer.groovy
@@ -3,13 +3,15 @@ package groovy.ui
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpHandler
 import com.sun.net.httpserver.HttpServer
+import groovy.transform.CompileStatic
 
 import java.util.concurrent.Executors
 
 /**
  * SimpleHTTPServer for Groovy, inspired by Python's SimpleHTTPServer
  */
-class SimpleHTTPServer {
+@CompileStatic
+public class SimpleHTTPServer {
     private HttpServer server;
     private int port;
     private String contextRoot;
@@ -21,19 +23,23 @@ class SimpleHTTPServer {
 
     public SimpleHTTPServer(final int port, final String contextRoot, final String docBase) {
         this.port = port;
-        this.contextRoot = contextRoot;
+        this.contextRoot = contextRoot.startsWith("/") ? contextRoot : "/$contextRoot";
         this.docBase = docBase;
 
         server = HttpServer.create(new InetSocketAddress(port), 0);
         server.setExecutor(Executors.newCachedThreadPool());
-        server.createContext(contextRoot, new HttpHandler() {
+        server.createContext(this.contextRoot, new HttpHandler() {
             @Override
             void handle(HttpExchange exchg) throws IOException {
                 BufferedOutputStream bos = new BufferedOutputStream(exchg.getResponseBody());
                 byte[] content = null;
 
                 try {
-                    content = new File("${docBase}${exchg.getRequestURI()}".trim()).bytes;
+                    String uri = exchg.getRequestURI();
+                    String path =
+                            '/' != SimpleHTTPServer.this.contextRoot && uri.startsWith(SimpleHTTPServer.this.contextRoot) ? uri.substring(SimpleHTTPServer.this.contextRoot.length()) : uri;
+
+                    content = new File("${docBase}${path}".trim()).bytes;
                     exchg.sendResponseHeaders(HttpURLConnection.HTTP_OK, content.length);
                     bos.write(content);
                 } catch (Exception e) {
@@ -50,7 +56,7 @@ class SimpleHTTPServer {
 
     public void start() {
         server.start();
-        println "HTTP Server is listening on port ${this.port}"
+        println "HTTP Server" + ('/' == contextRoot ? '' : "[$contextRoot]") + " is listening on port ${this.port}"
     }
 
     public static void main(String[] args) {
